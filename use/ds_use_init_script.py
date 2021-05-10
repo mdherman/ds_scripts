57b36e47d29673a3de0a816ee4a89fd967d7835f
Martin Mæland <martinmaeland@outlook.com>
2021-05-10 14:46:31 +0200

    init script for deployment unit

---
+++ use/ds_use_init_script.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+try:
+    # Get current file path
+    file_path = sys.path[0]
+
+    # Reading use_init file
+    system_setup = open(file_path + "/use_init.txt")
+    for line in system_setup:
+        if "use: " in line:
+            space = line.find(" ", 0, len(line))
+            use_number = int(line[space:])
+            use_namespace = str(use_number)
+
+    if (use_number > 10):
+        os.system("ros2 run ds_ros2_use use_transfer --ros-args -r __ns:=/use_" + use_namespace)
+    elif (use_number in range(1,10)):
+        os.system("ros2 run ds_ros2_use use_transfer --ros-args -r __ns:=/use_0" + use_namespace)
+    else:
+        print("Invalid number of use..")
+except:
+    print("Could not read 'use_init.txt'..")
